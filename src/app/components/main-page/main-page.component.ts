2410123a4d3552038c740afccaf256dc131be678
Hedgehuug <47126622+Hedgehuug@users.noreply.github.com>
2020-01-24 19:19:33 +0100

    5.2

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -19,14 +19,14 @@ export class MainPageComponent implements OnInit {
   
   constructor(public bo: BreakpointObserver) {
     this.skillsList = [
-      {name:'Angular',source:"./../../../assets/Imported_Components/icons8/angular.png"},
-      {name:'JavaScript',source:"./../../../assets/Imported_Components/icons8/js.png"},
-      {name:'html5',source:"./../../../assets/Imported_Components/icons8/html.png"},
-      {name:'css3',source:"./../../../assets/Imported_Components/icons8/css.png"},
-      {name:'Python',source:"./../../../assets/Imported_Components/icons8/python.png"},
-      {name:'Adobe XD',source:"./../../../assets/Imported_Components/icons8/xd.png"},
-      {name:'Photoshop',source:"./../../../assets/Imported_Components/icons8/photoshop.png"},
-      {name:'Github',source:"./../../../assets/Imported_Components/icons8/github.png"}
+      {name:'Angular',source:"../../../assets/Imported_Components/icons8/angular.png"},
+      {name:'JavaScript',source:"../../../assets/Imported_Components/icons8/js.png"},
+      {name:'html5',source:"../../../assets/Imported_Components/icons8/html.png"},
+      {name:'css3',source:"../../../assets/Imported_Components/icons8/css.png"},
+      {name:'Python',source:"../../../assets/Imported_Components/icons8/python.png"},
+      {name:'Adobe XD',source:"../../../assets/Imported_Components/icons8/xd.png"},
+      {name:'Photoshop',source:"../../../assets/Imported_Components/icons8/photoshop.png"},
+      {name:'Github',source:"../../../assets/Imported_Components/icons8/github.png"}
     ]
     if (this.bo.isMatched(this.bpList)) {
         this.isPhone = true;
